0684b67d26f3f91a6d032e734420d91641ea5c81
ecormaksin <truth_jp_4133@yahoo.co.jp>
2019-09-14 22:29:09 +0900

    テストを調整したが、そもそも外部サイトはテストできないことが判明

--- test-by-cypress/cypress/integration/test.spec.js
+++ test-by-cypress/cypress/integration/test.spec.js
@@ -1,8 +1,11 @@
-describe('test google search', () => {
-	it('acess to google', () => {
-		cy.visit('https://www.google.co.jp/')
+describe('IT用語辞典の検索', () => {
+	it('IT用語辞典の検索', () => {
+		cy.visit('http://e-words.jp/')
 
-		cy.get('input[name=q]')
-			.type('lassic{enter}')
+		cy.get('input[name=search]')
+			.type('CI')
+		
+		cy.get('.gsc-search-button .gsc-search-button-v2')
+			.click()
 	})
 })
