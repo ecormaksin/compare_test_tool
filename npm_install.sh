7cad6a993b2129457d26a66a7e9b12a15e3296e0
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2019-09-30 17:23:02 +0900

    npm installがAnsibleでインストールできなかったのでタスクから削除

--- npm_install.sh
+++ npm_install.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-ANSIBLE_ROOT_PATH=`dirname $0`
-cd $ANSIBLE_ROOT_PATH
+PROJECT_ROOT_PATH=`dirname $0`
+cd $PROJECT_ROOT_PATH
 pwd
 
 for DIR_NAME in test-by-cypress test-by-puppeteer test-by-selenium
 do
-    cd $ANSIBLE_ROOT_PATH
-    cd ../
+    cd $PROJECT_ROOT_PATH
     cd $DIR_NAME
     npm install
 done
 
-cd $ANSIBLE_ROOT_PATH
+cd $PROJECT_ROOT_PATH
 touch /tmp/npm_install_executed
